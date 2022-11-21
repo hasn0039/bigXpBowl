680405271e6a1080bdd054af3db743caa03de38d
hasnainahmad <hasn0039@stud.kea.dk>
2022-11-21 14:40:56 +0100

    s

---
+++ src/main/java/com/example/bigxpbowl/BigXpBowlApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigxpbowl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigXpBowlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigXpBowlApplication.class, args);
+    }
+
+}
